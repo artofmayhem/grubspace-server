0f7b6afdce62c6e86d83c98c1770dd09d7a43177
artofmayhem <chef_tony_miller@yahoo.com>
2021-08-29 11:33:25 -1000

    debugging

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,50 @@
-// Update with your config settings.
-require("dotenv").config();
+const pg = require('pg')
+//const parse = require("pg-connection-string").parse;
 
-const pg = require("pg");
+// // Parse the environment variable into an object containing User, Password, Host, Port etc at separate key-value pairs
+//const pgconfig = parse(process.env.DATABASE_URL);
 
+// // Add SSL setting to default environment variable on a new key-value pair (the value itself is an object)
+//pgconfig.ssl = { rejectUnauthorized: false };
 
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+// }
+console.log(">>>>>>>>>>>>>>> process.env.DATABASE_URL: ", process.env.DATABASE_URL);
 const sharedConfig = {
-  client: "pg",
-  migrations: { directory: "./data/migrations" },
-  seeds: { directory: "./data/seeds" },
-};
+  client: 'pg',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
 
 module.exports = {
   development: {
-    // ...sharedConfig,
-    client: "sqlite3",
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" },
-    connection: {
-      filename: "./data/development.db3",
+      client: 'sqlite3',
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+  connection: {
+    filename: "./data/development.db3"
+  },
+  useNullAsDefault: true,
+  pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys=ON", done);
+      },
     },
-    useNullAsDefault: true,
-    // pool: {
-    //   afterCreate: (conn, done) => {
-    //     conn.run("PRAGMA foreign_keys=ON", done);
-    //   },
-    // },
+
   },
   testing: {
     ...sharedConfig,
-    connection: process.env.TEST_DATABASE_URL,
+    connection: process.env.TESTING_DATABASE_URL,
   },
-
   production: {
     ...sharedConfig,
     connection: {
-      connectionString: process.env.DATABASE_URL ,
-      ssl: { rejectUnauthorized: false },
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+     },
+     pool: { min: 2, max: 10 },
   },
-};
+}
